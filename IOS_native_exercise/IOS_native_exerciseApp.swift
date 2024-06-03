72c8c785cf4530c135627938655545cf170541ed
GIL ALFREDO CASIMIRO RAMI <gil.casimiro@elektra.com.mx>
2024-06-03 13:26:03 -0600

    Initial Commit

---
+++ IOS_native_exercise/IOS_native_exerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_native_exerciseApp.swift
+//  IOS_native_exercise
+//
+//  Created by Gil casimiro on 03/06/24.
+//
+
+import SwiftUI
+
+@main
+struct IOS_native_exerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
